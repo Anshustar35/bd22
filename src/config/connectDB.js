94a083ea30e5a0d2c7bf73df87a4f6759bfe8f38
Anshustar35 <119737571+Anshustar35@users.noreply.github.com>
2024-05-20 08:10:50 +0530

    Update connectDB.js

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,10 +1,10 @@
-import mysql from 'mysql2/promise';
+const mysql = require('mysql2/promise');
 
-const pool = mysql.createPool({
-    host: 'bpjl5hl7rd3dknngsday-mysql.services.clever-cloud.com',
-    user: 'ueie7f1fmz8ovhp8',
-    password: '0DprGhsYEEMsxmwdon5U',
-    database: 'bpjl5hl7rd3dknngsday'
+const connection = mysql.createPool({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
 });
 
-export default pool;
+export default connection;
